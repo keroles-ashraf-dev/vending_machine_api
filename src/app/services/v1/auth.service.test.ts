1a4d3a6cd28f731f2975b339d6a2bf5ed270766d
keroles <keroles971005@gmail.com>
2024-04-12 20:04:31 +0200

    add tests

--- src/app/services/v1/auth.service.test.ts
+++ src/app/services/v1/auth.service.test.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { BaseUserRepo } from 'app/repositories/v1/user.repo';
 import { BaseUserRefreshTokenRepo } from 'app/repositories/v1/user.refresh.token.repo';
 import { AuthService } from './auth.service';
@@ -6,104 +7,112 @@ import { ErrorType, HttpStatusCode, UserRole } from 'utils/type';
 import User from 'app/models/user.model';
 import { BaseJWT } from 'helpers/jwt';
 
-// jest.mock('app/repositories/v1/user.repo', () => {
-//     return {
-//         BaseUserRepo: jest.fn().mockImplementation(() => {
-//             return {
-//                 findOne: findOne
-//             };
-//         })
-//     };
-// });
-// jest.mock('app/repositories/v1/user.refresh.token.repo');
-// jest.mock('helpers/jwt');
-
-const findOne = jest.fn().mockImplementation(() => {
-    return null;
-});
-
-const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
-    findOne: findOne,
-    findAll: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-    delete: jest.fn(),
-};
-
-const mockedUserRefreshTokenRepo: jest.Mocked<BaseUserRefreshTokenRepo> = {
-    create: jest.fn(),
-    findOne: jest.fn(),
-    verifyExpiration: jest.fn(),
-    delete: jest.fn(),
-};
-
-const mockedJWT: jest.Mocked<BaseJWT> = {
-    sign: jest.fn(),
-    verify: jest.fn(),
-};
-
-// beforeEach(() => {
-//     mockedUserRepo.mockClear();
-//     mockedUserRefreshTokenRepo.mockClear();
-//     mockedJWT.mockClear();
-// });
-
 describe('login', () => {
-    let authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+    const mockedUserRefreshTokenRepo: jest.Mocked<BaseUserRefreshTokenRepo> = {
+        create: jest.fn(),
+        findOne: jest.fn(),
+        verifyExpiration: jest.fn(),
+        delete: jest.fn(),
+    };
+    
+    const mockedJWT: jest.Mocked<BaseJWT> = {
+        sign: jest.fn(),
+        verify: jest.fn(),
+    };
+    
+    const username = faker.internet.userName();
+    const password = faker.internet.password();
+    const accessToken = faker.string.uuid();
+    const refreshToken = faker.string.uuid();
+    
+    const user = User.build({
+        id: 1,
+        username: username,
+        password: password,
+        role: UserRole.BUYER,
+        deposit: 0.0,
+    });
 
     it('it should throw error if user not found', async () => {
-        await expect(authServie.login('john', '123456')).rejects.toThrow(
+        const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
+            findOne: jest.fn().mockResolvedValue(null),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+        };
+        const authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+
+        await expect(authServie.login(username, password)).rejects.toThrow(
             new ApiError(ErrorType.GENERAL_ERROR, HttpStatusCode.NOT_FOUND, 'User not found', true)
         );
     });
 
-    authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
-    authServie.isUserPasswordValid = jest.fn().mockResolvedValue(false);
     it('it should throw error if user password not valid', async () => {
-        await expect(authServie.login('john', '123456')).rejects.toThrow(
+        const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
+            findOne: jest.fn().mockResolvedValue(user),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+        };
+        const authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+        authServie.isUserPasswordValid = jest.fn().mockResolvedValue(false);
+
+        await expect(authServie.login(username, password)).rejects.toThrow(
             new ApiError(ErrorType.GENERAL_ERROR, HttpStatusCode.UNAUTHORIZED, 'Invalid username or password')
         );
     });
 
-    authServie.isUserPasswordValid = jest.fn().mockResolvedValue(false);
-    it('it should throw error if user password not valid', async () => {
-        await expect(authServie.login('john', '123456')).rejects.toThrow(
-            new ApiError(ErrorType.GENERAL_ERROR, HttpStatusCode.UNAUTHORIZED, 'Invalid username or password')
-        );
-    });
-
-    authServie.generateAccessToken = jest.fn().mockResolvedValue(null);
     it('it should throw error if could not generate access token', async () => {
-        await expect(authServie.login('john', '123456')).rejects.toThrow(
+        const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
+            findOne: jest.fn().mockResolvedValue(user),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+        };
+        const authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+        authServie.isUserPasswordValid = jest.fn().mockResolvedValue(true);
+        authServie.generateAccessToken = jest.fn().mockReturnValue(null);
+
+        await expect(authServie.login(username, password)).rejects.toThrow(
             new ApiError(ErrorType.GENERAL_ERROR, HttpStatusCode.INTERNAL_SERVER_ERROR, 'something wrong happend, try again later')
         );
     });
 
-    authServie.createRefreshToken = jest.fn().mockResolvedValue(null);
     it('it should throw error if could not create refresh token', async () => {
-        await expect(authServie.login('john', '123456')).rejects.toThrow(
+        const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
+            findOne: jest.fn().mockResolvedValue(user),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+        };
+        const authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+        authServie.isUserPasswordValid = jest.fn().mockResolvedValue(true);
+        authServie.generateAccessToken = jest.fn().mockReturnValue(accessToken);
+        authServie.createRefreshToken = jest.fn().mockResolvedValue(null);
+
+        await expect(authServie.login(username, password)).rejects.toThrow(
             new ApiError(ErrorType.GENERAL_ERROR, HttpStatusCode.INTERNAL_SERVER_ERROR, 'something wrong happend, try again later')
         );
     });
 
-    const user = User.build({
-        id: 1,
-        username: 'john',
-        password: '123456',
-        role: UserRole.BUYER,
-        deposit: 0.0,
-    });
-    const accessToken = 'abcdefghjklmnopq';
-    const refreshToken = 'wcvedwfcwcfvewrfw';
-
-    //mockedUserRepo.findOne = jest.fn().mockResolvedValue(user);
-    // @ts-ignore
-    authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
-    authServie.isUserPasswordValid = jest.fn().mockResolvedValue(true);
-    authServie.generateAccessToken = jest.fn().mockResolvedValue(accessToken);
-    authServie.createRefreshToken = jest.fn().mockResolvedValue(refreshToken);
     it('it should return object of user, access token and refresh token', async () => {
-        await expect(authServie.login('john', '123456')).resolves.toEqual({
+        const mockedUserRepo: jest.Mocked<BaseUserRepo> = {
+            findOne: jest.fn().mockResolvedValue(user),
+            findAll: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+        };
+        const authServie = new AuthService(mockedUserRepo, mockedUserRefreshTokenRepo, mockedJWT);
+        authServie.isUserPasswordValid = jest.fn().mockResolvedValue(true);
+        authServie.generateAccessToken = jest.fn().mockReturnValue(accessToken);
+        authServie.createRefreshToken = jest.fn().mockResolvedValue(refreshToken);
+
+        await expect(authServie.login(username, password)).resolves.toEqual({
             user: user,
             accessToken: accessToken,
             refreshToken: refreshToken,
